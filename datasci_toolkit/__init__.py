f6615c30f12948024af3482f57c338674e2907a7
Daniel Herman <daniel.herman@protonmail.com>
2026-04-05 11:08:09 +0200

    feat: export ShapImportance, ShapRFE, plot_shap_elimination from package

--- datasci_toolkit/__init__.py
+++ datasci_toolkit/__init__.py
@@ -1,4 +1,5 @@
 from datasci_toolkit.bin_editor import BinEditor
+from datasci_toolkit.feature_elimination import ShapImportance, ShapRFE, plot_shap_elimination
 from datasci_toolkit.bin_editor_widget import BinEditorWidget
 from datasci_toolkit.grouping import StabilityGrouping, WOETransformer
 from datasci_toolkit.metrics import BootstrapGini, feature_power, gini, gini_by_period, iv, ks, lift, lift_by_period, plot_metric_by_period
@@ -35,4 +36,7 @@ __all__ = [
     "gini_by_period",
     "lift_by_period",
     "plot_metric_by_period",
+    "ShapImportance",
+    "ShapRFE",
+    "plot_shap_elimination",
 ]
---
+++ datasci_toolkit/feature_elimination/__init__.py
@@ -0,0 +1,5 @@
+from datasci_toolkit.feature_elimination._plot import plot_shap_elimination
+from datasci_toolkit.feature_elimination.elimination import ShapRFE
+from datasci_toolkit.feature_elimination.importance import ShapImportance
+
+__all__ = ["ShapImportance", "ShapRFE", "plot_shap_elimination"]
